e1df7497273c7314e3cc2880aa7144c31a5b1942
mohamad baker tarhini <tarhinimohammadbaker@gmail.com>
2024-10-26 18:09:26 +0300

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+n=int(input("please enter a number: "))
+i=0
+j=1
+t=0
+if n==0:
+    print(0)
+elif n==1:
+    print(0)
+    print(1)
+else:
+    print(0)
+    print(1)
+    k=2
+    while k<n:
+        t=i+j
+        i=j
+        j=t
+        print(t)
+        k+=1
+    
